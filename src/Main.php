063f693deca86f3c5d4f291f9a53abf412c0a13d
afshin hatampour <afshin89hatampour@gmail.com>
2021-08-25 19:15:39 +0430

    using httpguzzle in src

--- src/Main.php
+++ src/Main.php
@@ -2,8 +2,17 @@
 
 namespace Afs\Src;
 
+use GuzzleHttp\Client;
+
 class Main {
+    private $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = new Client();
+    }
     public function sayHello() {
-        return 'hello world';
+        $data = $this->httpClient->get('https://jsonplaceholder.typicode.com/todos/1');
+        return $data->getBody();
     }
 }
